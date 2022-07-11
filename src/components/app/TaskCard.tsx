fc4abf13e0148d2d3ff7a000e9d117e72e17d7ff
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2022-07-11 19:12:08 +0200

    feat: add toggle mutation to task card

--- src/components/app/TaskCard.tsx
+++ src/components/app/TaskCard.tsx
@@ -1,10 +1,13 @@
 import { Project, Task } from "@prisma/client";
+import { FaTasks } from "react-icons/fa";
+import { BsCheckCircle, BsFillCheckCircleFill } from "react-icons/bs";
 import { RiDeleteBinFill } from "react-icons/ri";
 import { useQueryClient } from "react-query";
 import { rem, styled } from "../../styles/stitches.config";
 import { trpc } from "../../utils/trpc";
 import { Button } from "../common/button";
 import { Card as AppCard } from "../common/card";
+import { Flex } from "../common/common";
 import { Spinner } from "../common/spinner";
 import { Description, Title } from "../common/text";
 
@@ -23,34 +26,64 @@ export const TaskCard = ({ task, project }: Props) => {
       }
     },
   });
+  const { mutate: mutateToggle, isLoading: toggleLoading } = trpc.useMutation(
+    ["task.toggle"],
+    {
+      onSuccess() {
+        if (project?.id !== undefined) {
+          v.invalidateQueries(["project.get", { id: project?.id }]);
+        } else {
+          v.invalidateQueries(["task.getAll"]);
+        }
+      },
+    },
+  );
 
   return (
     <s.Card>
       <s.Header>
         <Flex gap={2} css={{ alignItems: "center" }}>
           <FaTasks size={20} color={task.color} />
-        <Title>{task.title}</Title>
+          <Title>{task.title}</Title>
         </Flex>
       </s.Header>
 
-        <s.HeaderActions>
-          <Button size="sm" variant="delete" onClick={() => mutate(task.id)}>
-            {isLoading ? (
-              <Spinner size="small" color="white" />
-            ) : (
-              <RiDeleteBinFill />
-            )}
-          </Button>
-        </s.HeaderActions>
-      </s.Header>
-
       <Description>{task.description}</Description>
+
+      <s.Actions>
+        <Button
+          size="sm"
+          variant={task.status === "DONE" ? "success" : "delete"}
+          onClick={() =>
+            mutateToggle({
+              id: task.id,
+              status: task.status === "DONE" ? "TODO" : "DONE",
+            })
+          }
+        >
+          {toggleLoading ? (
+            <Spinner size="small" color="white" />
+          ) : task.status === "DONE" ? (
+            <BsFillCheckCircleFill />
+          ) : (
+            <BsCheckCircle />
+          )}
+        </Button>
+        <Button size="sm" variant="delete" onClick={() => mutate(task.id)}>
+          {isLoading ? (
+            <Spinner size="small" color="white" />
+          ) : (
+            <RiDeleteBinFill />
+          )}
+        </Button>
+      </s.Actions>
     </s.Card>
   );
 };
 
 namespace s {
   export const Card = styled(AppCard, {
+    position: "relative",
     minHeight: rem(150),
 
     "&:hover": {
@@ -71,8 +104,10 @@ namespace s {
     paddingBottom: "$4",
   });
 
-  export const HeaderActions = styled("div", {
-    height: rem(25),
+  export const Actions = styled("div", {
+    position: "absolute",
+    bottom: "$2",
+    right: "$2",
     display: "flex",
   });
 }
