768ce25f778d2329d8df29d1da298d379586b9a0
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2022-07-14 16:38:01 +0200

    feat: add ghost button variant

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,20 +1,36 @@
 import React from "react";
 
-type ButtonVariant =
-  | "primary"
-  | "secondary"
-  | "white"
-  | "success"
-  | "delete"
-  | "navbar"
-  | "ghost";
+const BASE =
+  "flex items-center justify-center gap-2 py-2 px-6 focus:outline-none rounded border font-semibold";
 
-type Props = { variant?: ButtonVariant } & React.HTMLProps<HTMLButtonElement>;
-export const Button = ({ children, className, onClick }: Props) => {
+type Props = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+export const Button = ({ children, className, onClick, ...props }: Props) => {
   return (
     <button
+      {...props}
       onClick={onClick}
-      className={`text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg ${className}`}
+      className={`${BASE} text-white bg-indigo-500 hover:bg-indigo-600 ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export const GhostButton = ({
+  children,
+  className,
+  onClick,
+  ...props
+}: Props) => {
+  return (
+    <button
+      {...props}
+      onClick={onClick}
+      className={`${BASE} text-slate-900 bg-white hover:bg-slate-50 ${className}`}
     >
       {children}
     </button>
