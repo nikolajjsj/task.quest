894dbb197e71c53903f20aa0294886c60d13d95e
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2022-07-22 09:44:50 +0200

    feat: add user router to get a user/search for users

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -4,12 +4,14 @@ import superjson from "superjson";
 import { taskRouter } from "./task";
 import { authRouter } from "./auth";
 import { projectRouter } from "./project";
+import { userRouter } from "./user";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("auth.", authRouter)
   .merge("task.", taskRouter)
-  .merge("project.", projectRouter);
+  .merge("project.", projectRouter)
+  .merge("user.", userRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
